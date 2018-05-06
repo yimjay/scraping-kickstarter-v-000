900f7542a443f19316c3e4c778877daf5487a9c6
Ji Yim <gsooyim@gmail.com>
2018-05-06 04:03:59 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,14 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  
+  projects
 end
 
 
